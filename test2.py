05d04b2df49037549a44e86abf2ce2af3ccf7f3a
B-Qq <33053732+B-Qq@users.noreply.github.com>
2018-02-01 21:49:33 +0800

    newest

--- test2.py
+++ test2.py
@@ -13,6 +13,7 @@ import tkinter.messagebox
 import time,threading
 from bs4 import BeautifulSoup
 from queue import Queue
+from multiprocessing import Process
 import os
 
 #全局变量
@@ -29,13 +30,13 @@ en = ''
 num = 0
 
 def get_url(max_behot_time, AS, CP):
-    url = 'https://www.toutiao.com/api/pc/feed/?category=news_society&utm_source=toutiao&widen=1' \
+    url = 'https://www.toutiao.com/api/pc/feed/?category=news_hot&utm_source=toutiao&widen=1' \
           '&max_behot_time={0}' \
           '&max_behot_time_tmp={0}' \
           '&tadrequire=true' \
           '&as={1}' \
           '&cp={2}' \
-          '&_signature=pxpB2xAc.VAK8B9apKgvHKcaQa'.format(max_behot_time, AS, CP)
+          '&_signature=nuE3NAAAxJwzC2m1mvezbZ7hNy'.format(max_behot_time, AS, CP)
     return url
 
 
@@ -65,7 +66,6 @@ def get_ASCP():
 
 
 def download(title,comment_count, news_url):
-    print('------------------------------开始下载----------------------------------')
     req = request.urlopen(news_url)
     if req.getcode() != 200:
         print('返回状态有错误,不继续解析')
@@ -76,6 +76,7 @@ def download(title,comment_count, news_url):
     if len(result) == 0:
         print ("文章无内容!!!")
         return 0
+    print('------------------------------开始下载----------------------------------')
     p = re.compile(r'&lt;p&gt;[\s\S]*?&lt;/p&gt;')
     result = p.findall(str(result))
     title = title.replace(':', '')
@@ -106,7 +107,6 @@ def download(title,comment_count, news_url):
             i = re.sub('&lt;span&gt;|&lt;/span&gt;','',i)
             file_object.write(i)
             file_object.write('\n')
-     #print('正在爬取')
 
 
 def get_item(url):
@@ -140,23 +140,18 @@ def get_item(url):
         download(title_q.get(),comment_count_q.get(),news_url_q.get())
         time.sleep(2)
 
-
-def thread_it(func):
-	t = threading.Thread(target=func)
-	t.setDaemon(True)
-	t.start()
-	
 #供开始按钮调用
 def show():
     global var
     global Run_flag
-    global p
     Run_flag = True
     if var != "":
         print('----------------开始爬取文章-------------------')
-        start_spider()
+        t1 = threading.Thread(target=start_spider,name='start_spider')
+        t1.start()
     else:
         tkinter.messagebox.showerror(title='错误',message='请输入要爬取内容的评论数,并点击确认')
+
 
 #供停止按钮调用
 def stop():
@@ -173,8 +168,8 @@ def start_spider():
     global path
     global var1,var2,var3
     global Run_flag
-    toutiao_flag = 0
     x = 0
+    toutiao_flag = 0
     weibo_flag = 0
     all_flag = 0
     if var1.get() == 1:
@@ -199,12 +194,10 @@ def start_spider():
             max_behot_time = 0
             AS, CP = get_ASCP()
             url = get_url(max_behot_time, AS, CP)
-            print('json_url::',url)
             get_item(url)
             time.sleep(1)
     tkinter.messagebox.showinfo(title='完成',message='停止爬取数据，文件存储在'+path+'中,点击\' OK \'退出')
     exit()
-
 
 #获取输入的爬去评论数
 def get_str():
@@ -226,7 +219,7 @@ def tkin_show():
     var1=tk.IntVar()
     tk.Checkbutton(top,text='今日头条',bg='yellow',variable=var1,onvalue=1,offvalue=0,width=10).grid(row=0,sticky='W',column=0)
     var2=tk.IntVar()
-    tk.Checkbutton(top,text='微博',bg='red',variable=var2,width=10).grid(row=1,sticky='W',column=0)
+    tk.Checkbutton(top,text='简书',bg='red',variable=var2,width=10).grid(row=1,sticky='W',column=0)
     var3=tk.IntVar()
     tk.Checkbutton(top,text='全部',bg='green',variable=var3,width=10).grid(row=2,sticky='W',column=0)
 
@@ -235,22 +228,12 @@ def tkin_show():
     en = tk.Entry(top,width=9,bg='LightSkyBlue')
     en.grid(row=1,column=2,sticky = 'E')
     tk.Button(top,text='确定',command=get_str,width=7).grid(row=2,column=2,sticky='E')
-    tk.Button(top,text='开始',command = lambda:thread_it(show),width = 40,bg='blue').grid(row=4,column=0,columnspan=3,sticky='SE')
+    tk.Button(top,text='开始',command = show,width = 40,bg='blue').grid(row=4,column=0,columnspan=3,sticky='SE')
     tk.Button(top,text='停止',command = stop,width = 40,bg='blue').grid(row=5,column=0,columnspan=3,sticky='SE')
 
     top.mainloop()
 
-
 if __name__ == '__main__':
-    '''
-    t = threading.Thread(target=tkin_show(),name='tkin_show')
-    t.start()
-    t.join()
-    while True:
-        {
-
-        }
-    #t1 = threading.Thread(target=start_spider,name='start_spider')
-    #t1.start()
-    '''
     tkin_show()
+
+
